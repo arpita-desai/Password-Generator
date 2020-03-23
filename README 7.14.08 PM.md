46b1133ceeef8227d1fa9f18636b87cbdf53923d
Arpita Desai <patelarpita29@gmail.com>
2020-03-22 19:32:17 -0700

    Password generator using prompts

---
+++ README 7.14.08 PM.md
@@ -0,0 +1,19 @@
+# Password-Generator
+
+This appliction generates the random secure password.
+
+Acceptance Criteria: password lenght must be minimum 8 character.
+
+There are series of prompts for password criteria.
+    Prompt for password length.
+    prompt for asking lower letter in password.
+    prompt for asking upper letter in password.
+    prompt for numbers in password.
+    prompt for special character in password.
+    
+If all prompts are  not selected (cancled) then password will not generate.
+
+After selecting all prompts click on generate password button.
+
+    
+    
--- script.js
+++ script.js
@@ -1,19 +1,107 @@
-//DOM Element
 
-function generator(){
-    //set length of password
-    let lengthPassword = document.getElementById("passwordLong").value;
-    //password values
-    let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnpqrstuvwxyz1234567890!@#$%^&*()_+";
+//Dom element
 
-    let password = "";
+//var passwordDisplay = document.getElementById("display");
+var generate = document.getElementById("myBtn");
 
-    //create for loop for password 
-    for(var i = 0; i < lengthPassword; i++){
+//Ask users preferences
+
+var passwordLength = prompt("How long your password should be?");
+    if(passwordLength < 8 || passwordLength === null){
+            alert("Password length should be at least 8");
+            var passwordLength = prompt("How long your password should be?");
+    }    
+var lowerCase = confirm("Do you want lowercase letter in password?");
+var upperCase = confirm("Do you want uppercase letter in password?");
+var symbols = confirm("Do you want special character in password?");
+var numbers = confirm("Do you want numbers in password?");
+
+
+
+//creating function for random string generations
+
+
+function getRandomLower(){
+    return String.fromCharCode(Math.floor(Math.random()*26) + 97);
+}
+
+function getRandomUpper(){
+    return String.fromCharCode(Math.floor(Math.random()*26) + 65);
+}
+
+function getRandomNumber(){
+    return String.fromCharCode(Math.floor(Math.random()*10) + 48);
+}
+
+function getRandomSymbols(){
+    const symbols = "!@#$%^&*()_+<>?,.{}[]";
+    return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+generate.addEventListener("click", checkForPassword);
+
+const randomFunc = {
+	lower: getRandomLower,
+	upper: getRandomUpper,
+	numbers: getRandomNumber,
+	symbols: getRandomSymbols
+};
+
+//main function to generate password
+ function checkForPassword(){
+
+    var length = +passwordLength;
+    var hasLower, hasNumber, hasUpper, hasSymbol;
         
-        password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
-    }
+        if(lowerCase){
+             hasLower = 1;
+        }else hasLower = 0;
+    
+        if(upperCase){
+            hasUpper = 1;
+        }else hasUpper = 0;
+    
+        if(symbols){
+            hasSymbol = 1;
+        }else hasSymbol = 0;
 
-    //add password to display
+        if(numbers){
+             hasNumber = 1;
+        }else hasNumber = 0;
+    
+       console.log(length, hasLower, hasNumber, hasUpper, hasSymbol);
+
+        //passwordDisplay.innerText = generator(hasLower, hasUpper, hasNumber, hasSymbol, length);
+        generator(hasLower, hasUpper, hasNumber, hasSymbol, length);
+       
+ }
+
+ //For password generator
+ function generator(lower, upper, numbers, symbols, length){
+
+    var password = "";
+
+    const typesCount = lower + upper + numbers + symbols;
+    
+    console.log(typesCount);
+    
+    const typesArr = [{lower}, {upper}, {numbers}, {symbols}].filter(item => Object.values(item)[0]);
+	
+	// Doesn't have a selected type
+	if(typesCount === 0) {
+		return '';
+	}
+	
+	// create a loop
+	for(let i=0; i<length; i+=typesCount) {
+		typesArr.forEach(type => {
+			const funcName = Object.keys(type)[0];
+			password += randomFunc[funcName]();
+		});
+	}
+	
+	//const finalPassword = password.slice(0, length);
+   
     document.getElementById("display").value = password;
 }
+    
